c16f970f747440d74f754b8305ff2b6d6c5bface
Gilbert Maystre <gilbert@maystre.ch>
2015-12-17 13:32:32 -0500

    First commit

---
+++ src/main/java/dbproject/Bootstrap.java
@@ -0,0 +1,14 @@
+/*
+ *	Author:      Gilbert Maystre
+ *	Date:        Dec 17, 2015
+ */
+
+package dbproject;
+
+public class Bootstrap {
+
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+    
+}
